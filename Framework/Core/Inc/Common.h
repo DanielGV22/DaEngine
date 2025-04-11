d1290be2a56231547b7d952d7742410a1005b1c2
LCV2342892 <LCV2342892@LCV227-14>
2025-04-10 21:51:31 -0700

    Week 1 Setup

---
+++ Framework/Core/Inc/Common.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
+// Win32 Header Files
+#include<objbase.h>
+#include<Windows.h>
+
+// std Header Files
+#include<algorithm>
+#include<array>
+#include<atomic>
+#include<chrono>
+#include<cstdio>
+#include<cstdlib>
+#include<cstdint>
+#include<filesystem>
+#include<functional>
+#include<list>
+#include<map>
+#include<memory>
+#include<optional>
+#include<string>
+#include<unordered_map>
+#include<variant>
+#include<vector>
---
+++ VGP242/01_HelloWindow/WinMain.cpp
@@ -0,0 +1,6 @@
+#include<DaEngine/Inc/DaEngine.h>
+
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int) 
+{
+	return 0;
+}
